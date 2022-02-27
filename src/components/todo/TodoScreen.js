d2b4e32d5d172c70c005da7603a65ba6f90dfe6d
stargazing4122 <jadenyeiden@gmail.com>
2022-02-26 20:41:26 -0500

    diseño home en construccion

--- src/components/todo/TodoScreen.js
+++ src/components/todo/TodoScreen.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const TodoScreen = () => {
+  const style = {
+    color: 'azure',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
   return (
-    <div>
-      todo screen!
+    <div style={style}>  
+      <h2>Pagina en construcción...</h2>
+      <button className='btn btn-logout'>Logout</button>
     </div>
   )
 }
